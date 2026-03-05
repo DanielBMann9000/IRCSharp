7187c62132edd3a9ab78a54fed5944ba4cdc743b
Daniel Mann <dbmann@gmail.com>
2026-03-04 20:56:32 -0500

    refactor(tests): migrate TestHelpers to Moq - partial implementation

--- IrcSharp.Core.Tests.Unit/TestHelpers.cs
+++ IrcSharp.Core.Tests.Unit/TestHelpers.cs
@@ -1,5 +1,5 @@
 using Xunit;
-﻿using System;
+using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 using IrcSharp.Core.Connectivity;
 using IrcSharp.Core.Messages;
 using IrcSharp.Core.Messages.Interfaces;
-
+using Moq;
 
 namespace IrcSharp.Core.Tests.Unit;
     
@@ -18,8 +18,17 @@ namespace IrcSharp.Core.Tests.Unit;
             Action<IrcConnection, ManualResetEvent> registrationAction)
         {
             var mre = new ManualResetEvent(false);
-            var cm = new FakeSocketConnection();
-            using (var con = new IrcConnection(cm))
+            var mockSocket = new Mock<ISocketConnection>();
+22#HB|            mockSocket.Setup(s => s.Connected).Returns(true);
+23#TB|            mockSocket.Setup(s => s.ConnectAsync(It.IsAny<string>(), It.IsAny<int>())).Returns(Task.CompletedTask);
+24#VW|            mockSocket.Setup(s => s.SendMessageAsync(It.IsAny<ISendableMessage>())).Returns(Task.CompletedTask);
+25#ZN|            mockSocket.Setup(s => s.DisconnectAsync()).Returns(Task.CompletedTask);
+            using (var con = new IrcConnection(mockSocket.Object))
+            mockSocket.Setup(s => s.Connected).Returns(true);
+            mockSocket.Setup(s => s.ConnectAsync(It.IsAny<string>(), It.IsAny<int>())).Returns(Task.CompletedTask);
+            mockSocket.Setup(s => s.SendMessageAsync(It.IsAny<ISendableMessage>())).Returns(Task.CompletedTask);
+            mockSocket.Setup(s => s.DisconnectAsync()).Returns(Task.CompletedTask);
+            using (var con = new IrcConnection(mockSocket.Object))
             {
                 await con.ConnectAsync("foo", "bar", "baz", 0);
                 registrationAction(con, mre);
